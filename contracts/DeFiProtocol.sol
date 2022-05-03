471404b49a18315e643bb458ecbf80dbe97c4913
OhYeah68 <sylv.byl@gmail.com>
2022-05-03 22:54:53 +0200

    #4 Ajout du Smart Contract protocole

---
+++ contracts/DeFiProtocol.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.13;
+
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+/**
+ * @title Hello World DeFi Protocol
+ * @author The Hello World Team
+ * @notice This contract offer the possibility to stake ERC20 in Liquidity Pools to receive Hello World Token rewards
+ */
+contract DefiProtocol is Ownable {
+
+
+}
