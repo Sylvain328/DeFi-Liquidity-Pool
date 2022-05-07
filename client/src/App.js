1290468559293aa8f5a8cd2e738a389fe6ef2794
OhYeah68 <sylv.byl@gmail.com>
2022-05-07 22:57:06 +0200

    #8 - Debut front

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import DefiProtocol from "./contracts/DefiProtocol.json";
+import HWT from "./contracts/HWT.json";
 import getWeb3 from "./getWeb3";
+import Header from "./components/Header.js";
 
 import "./App.css";
 
@@ -17,9 +19,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = DefiProtocol.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        DefiProtocol.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -35,37 +37,12 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+      <Header account={this.state.account} isOwner={this.state.isOwner} />
     );
   }
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="Header">
+                <div>
+                    <h1>DeFi Pool Liquidity</h1>
+                </div>
+                <div>
+                    <div>{this.props.account}</div>
+                    <div className='StatusContainer'>
+                        <div className={`OwnerStatus ${this.props.isOwner ? "" : "HideComponent"}`}>owner</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
