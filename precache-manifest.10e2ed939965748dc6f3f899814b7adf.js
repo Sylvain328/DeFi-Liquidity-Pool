36107b14452521a2f8ffeb3058c3d200568f789b
Sylvain B <sylv.byl@gmail.com>
2022-05-14 16:27:33 +0200

    Updates

---
+++ precache-manifest.10e2ed939965748dc6f3f899814b7adf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c67a99c34e15a11ba6b6e9606050a2",
+    "url": "/DeFi-Liquidity-Pool/index.html"
+  },
+  {
+    "revision": "0887c57c9b18450bdf23",
+    "url": "/DeFi-Liquidity-Pool/static/css/main.f2ccd842.chunk.css"
+  },
+  {
+    "revision": "edde6a6d9ce852426af9",
+    "url": "/DeFi-Liquidity-Pool/static/js/2.11f87e8f.chunk.js"
+  },
+  {
+    "revision": "0887c57c9b18450bdf23",
+    "url": "/DeFi-Liquidity-Pool/static/js/main.8e2d3891.chunk.js"
+  },
+  {
+    "revision": "02a507fc5b8f65a774e6",
+    "url": "/DeFi-Liquidity-Pool/static/js/runtime-main.826460d9.js"
+  },
+  {
+    "revision": "e097b7fb9a5d0ac715494cbc5ce51ded",
+    "url": "/DeFi-Liquidity-Pool/static/media/back2.e097b7fb.jpg"
+  },
+  {
+    "revision": "89c8113d4206edb49b477a3939c86fe5",
+    "url": "/DeFi-Liquidity-Pool/static/media/neon2.89c8113d.ttf"
+  }
+]);
